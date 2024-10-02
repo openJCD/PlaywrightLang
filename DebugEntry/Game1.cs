197ba8e6983faebb34327b9d65583de4968768d8
openjcd <stuart.barton2017@gmail.com>
2024-10-02 17:05:35 +0100

    First commit (lexer stage)

---
+++ DebugEntry/Game1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using PlaywrightLang.LanguageServices;
+
+namespace PlaywrightLang;
+
+internal class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Tokeniser tokeniser;
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        tokeniser = new Tokeniser("script.pw");
+        List<Token> tokens = tokeniser.Tokenise();
+        Console.WriteLine("Tokens: ");
+        tokens.ForEach(t => Console.WriteLine(t.Type.ToString() + " " + t.Value?.ToString()));
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+        
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        
+        base.Draw(gameTime);
+    }
+}
---
+++ LanguageServices/Tokeniser.cs
@@ -0,0 +1,151 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+namespace PlaywrightLang.LanguageServices;
+
+public class Tokeniser
+{
+    private string _filepath;
+    private readonly char[] _codeChars;
+    private string _codeRaw;
+    private string _currentLine;
+    private int _readerIndex;
+    
+    public Tokeniser(string filepath)
+    {
+        _filepath = filepath;
+        Stream s = File.OpenRead(filepath);
+        StreamReader sr = new StreamReader(s);
+        _codeRaw = sr.ReadToEnd();
+        _codeChars = _codeRaw.ToCharArray();
+    }
+
+    public List<Token> Tokenise()
+    {
+        string _bufCurrent = "";
+        List <Token> tokens = new List<Token>();
+        while (Peek() != '\0')
+        {
+            char ch_consumed = Consume();
+            switch (ch_consumed)
+            {
+                case '\n' :
+                    tokens.Add(new Token(TokenType.Newline));
+                    break;
+                case '+':
+                    tokens.Add(new Token(TokenType.Plus));
+                    break;
+                case '-':
+                    tokens.Add(new Token(TokenType.Minus));
+                    break;
+                case '*':
+                    tokens.Add(new Token(TokenType.Multiply));
+                    break;
+                case '/':
+                    tokens.Add(new Token(TokenType.Divide));
+                    break;
+                default: break;
+            }
+            // handle string literals
+            if (ch_consumed == '"')
+            {
+                while (Peek() != '"' && Peek() != '\0')
+                {
+                    _bufCurrent += Consume();
+                    if (Peek() == '\n')
+                        Console.Error.WriteLine("Fatal error: Unterminated one-line string.");
+                }
+
+                Consume();
+                tokens.Add(new Token(TokenType.StringLiteral, _bufCurrent));
+                _bufCurrent = "";
+            }
+            
+            if (!char.IsLetterOrDigit(ch_consumed))
+                continue;
+            
+            if (char.IsLetter(ch_consumed))
+            {
+                _bufCurrent += ch_consumed;
+                
+                while (char.IsLetterOrDigit(Peek()))
+                    _bufCurrent += Consume();
+
+                switch (_bufCurrent)
+                {
+                    case "Fin":
+                        tokens.Add(new Token(TokenType.Exit));
+                        break;
+                    default:
+                        tokens.Add(new Token(TokenType.VariableName, _bufCurrent));
+                        break;
+                }
+                _bufCurrent = "";
+                
+                continue;
+            }
+            // handle strings of digits for int literals
+            if (char.IsDigit(ch_consumed))
+            { 
+                _bufCurrent = "";
+                _bufCurrent += ch_consumed;
+                while (char.IsDigit(Peek()))
+                {
+                    _bufCurrent += Consume();
+                }
+                tokens.Add(new Token(TokenType.IntLiteral, _bufCurrent));
+                _bufCurrent = "";
+                continue;
+            }
+
+        }
+        return tokens;
+    }
+    
+    char Peek(int ahead = 1)
+    {
+        if (_readerIndex + ahead >= _codeChars.Length)
+            return char.MinValue;
+        return _codeChars.ElementAtOrDefault(_readerIndex);
+    }
+
+    public char Consume()
+    {
+        char c = _codeChars.ElementAtOrDefault(_readerIndex++);
+        return c;
+    }
+}
+
+public enum TokenType
+{
+    Exit,
+    StringLiteral,
+    IntLiteral,
+    Newline,
+    Plus,
+    Minus, 
+    Multiply,
+    Divide,
+    VariableName,
+}
+
+public struct Token
+{
+    public TokenType Type;
+    public string? Value;
+    
+    public Token(TokenType type)
+    {
+        Type = type;
+    }
+
+    public Token(TokenType type, string? value)
+    {
+        Type = type;
+        Value = value;
+    }
+}
