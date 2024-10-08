9efdab583f4c77b78df41f183691e40c5e5f137f
openjcd <stuart.barton2017@gmail.com>
2024-10-08 14:06:55 +0100

    further work on precedence climbing

--- LanguageServices/Parser.cs
+++ LanguageServices/Parser.cs
@@ -1,6 +1,11 @@
 ﻿#nullable enable
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
+using System.Text;
+
 namespace PlaywrightLang.LanguageServices;
 
 #region nodes
@@ -54,38 +59,103 @@ public class Parser
             { TokenType.Multiply, 2 },
             { TokenType.Divide, 2 }
         };
-    public AST AbstractSyntaxTree { get; private set; }
     private List<Token> _tokens;
     private int _tokenIndex;
+    private int _currentLine=1;
+    
+    public Dictionary<string, PwObject> Objects = new Dictionary<string, PwObject>();
+    
     public Parser(List<Token> tokens)
     {
         _tokens = tokens;
     }
     Token ComputeExpression(int initialPrecedence = 1)
     {
+        Token resultLeft = ComputeNumber();
+        
         while (Peek().Type != TokenType.Null)
         {
-            NodeExpr left_current;
-            Token consumed_token = Consume();
-            NodeExpr right_current;
-            
-            if (consumed_token.Type == TokenType.IntLiteral)
+            Token current = Consume();
+            // taken from pseudocode: while current token is a binary operator with precedence >= minimum precedence
+            if (GetPrec(current.Type) >= initialPrecedence)
             {
-                if (Peek(-1).Type == TokenType.Null)
-                {
-                    left_current = new IntLeaf();
-                }
+                int precedence = GetPrec(current.Type);
+                // Associativity assoc = GetAssoc(current.Type)
+                int nextPrecedence = GetPrec(current.Type) + 1;
+                Token resultRight = ComputeExpression(nextPrecedence);
+                Token resultOverall = Calculate(current.Type, resultLeft, resultRight);
             }
+            if (current.Type == TokenType.Newline)
+                _currentLine++;
+            
         }
+        return Token.None;
+    } 
+    Token ComputeNumber()
+    {
+        // taken from eli.thegreenplace.net, re-written in c# and for this 
+        // parser structure.
+        
+        // recursively handle expressions in brackets.
+        if (Peek().Type == TokenType.LParen)
+        {
+            // move to next token before computing expression inside brackets
+            Consume();
+            
+            // actually calculate expression inside brackets (recursively)
+            Token value = ComputeExpression(1);
+            
+            // throw an error in the event of 
+            if (Peek().Type != TokenType.RParen)
+                Console.Error.WriteLine($"Expected ')' but got '{Peek().Type}'");
+            
+            Consume();
+            return value;
+        } else if (Peek().Type == TokenType.Null)
+        {
+            Console.Error.WriteLine($"Got Null token (file ended unexpectedly)");
+        } 
+        else if (BinaryOps.Keys.Contains(Peek().Type)) // if the peeked token is actually a math operator...
+            ThrowError($"Got a binary operator {Peek().Type} when expecting int literal or bracketed expression.");
+        
+        else if (Peek().Type == TokenType.IntLiteral)
+            return Consume();
+        
+        // return a null token if all else fails
+        
+        ThrowError($"Failed to compute expression (unexpected error). Bailing out.");
         return Token.None;
     }
 
-    Token ComputeNumber()
+    int GetPrec(TokenType t)
     {
-        // TODO: change this
-        return new Token();
+        if (BinaryOps.Keys.Contains(t))
+            return BinaryOps[t];
+        else return 0;
     }
-    
+    Token Calculate(TokenType operand, Token left, Token right)
+    {
+        int lv = int.Parse(left.Value);
+        int rv = int.Parse(right.Value);
+        switch (operand)
+        {
+            case TokenType.Plus:
+                return new Token(TokenType.IntLiteral, (lv + rv).ToString());
+                break;
+            case TokenType.Minus: 
+                return new Token(TokenType.IntLiteral, (lv - rv).ToString());
+                break;
+            case TokenType.Multiply:
+                return new Token(TokenType.IntLiteral, (lv * rv).ToString());
+                break;
+            case TokenType.Divide:
+                return new Token(TokenType.IntLiteral, (lv / rv).ToString());
+                break;
+            default:
+                ThrowError("Found unexpected operand.");
+                return Token.None;
+        }
+    }
     /// <summary>
     /// Increment the current token index and get token
     /// </summary>
@@ -105,5 +175,11 @@ public class Parser
             return Token.None;
         
         return _tokens.ElementAt(_tokenIndex + ahead);
+    }
+
+    void ThrowError(string err)
+    {
+        StringBuilder message = new StringBuilder();
+        message.Append($"Parse error on line '{_currentLine}': {err}");
     }
 }
