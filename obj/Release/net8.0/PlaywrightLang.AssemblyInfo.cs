55f966909f83f9ee7f34f7882487090bd7c0de33
openjcd <stuart.barton2017@gmail.com>
2025-05-13 12:23:35 +0100

    remove grammar.md

--- obj/Release/net8.0/PlaywrightLang.AssemblyInfo.cs
+++ obj/Release/net8.0/PlaywrightLang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightLang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc94f7921702ac3abd666c5b26d257362fdc7c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24857c2b76a059b6dc8aeaa47a400eac21a311e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightLang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightLang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
